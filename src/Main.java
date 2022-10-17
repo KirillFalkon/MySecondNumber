57126c555ddd3e0ae9b0f73053f8b00a9862674b
Kyrylo_Falko <kirill1.falko@gmail.com>
2022-10-17 18:24:54 +0300

    My Second Try

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    private String name;
+
+    public void setName(String name) {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    }
+}
